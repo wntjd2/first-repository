5e900a2f28def7f8ef902c225ccb818d0448ed02
wntjd2 <wntjd3049@naver.com>
2023-09-13 15:26:45 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
+    print("Hello World!!\n")
+def print_KIT():
+    print("Kunoh NationalInstitue of Techonlogy.")
 
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
+
     
